5abaa46f9f46d0c5417182c17c64ac1963e63766
PARK SEONJU <101613808+mimimya@users.noreply.github.com>
2026-01-02 17:13:32 +0900

    Solve007 - 연속된 1 만들기

---
+++ solutions/SJ/solve007.java
@@ -0,0 +1,33 @@
+package solutions.SJ;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class solve007 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        String S = br.readLine();
+
+        String doubled = S + S;
+
+        int zeroCnt = 0;
+        int l = 0, answer = 0;
+
+        for (int r = 0; r < doubled.length(); r++) {
+            if (doubled.charAt(r) == '0') zeroCnt++;
+
+            while (zeroCnt > K ||  r - l + 1 > N)  { //  (r - l + 1 > N) : S=11111
+                if (doubled.charAt(l) == '0') zeroCnt--;
+                l++;
+            }
+
+            answer = Math.max(answer, r-l+1);
+        }
+
+        System.out.println(answer);
+    }
+}
